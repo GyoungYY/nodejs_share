e714177462e81cda1493a9a898434a5977235d41
yangguang <yangguang@bilibili.com>
2018-05-22 19:53:41 +0800

    demo

---
+++ demo/02CreateServer.js
@@ -0,0 +1,33 @@
+// 在回调函数中，除了可以使用request对象访问请求头数据外，还能把request对象当作一个只读数据流来访问请求体数据。
+let http = require('http');
+
+http.createServer(function (request, response) {
+    var body = [];
+
+    console.log(request.method);
+    console.log(request.headers);
+
+    request.on('data', function (chunk) {
+        body.push(chunk);
+    });
+
+    request.on('end', function () {
+        body = Buffer.concat(body);
+        console.log(body.toString());
+    });
+}).listen(8889);
+
+
+// 在回调函数中，除了可以使用response对象来写入响应头数据外，还能把response对象当作一个只写数据流来写入响应体数据。
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    request.on('data', function (chunk) {
+        response.write(chunk);
+    });
+
+    request.on('end', function () {
+        response.end();
+    });
+}).listen(8890);
--- nodejs/index.js
+++ nodejs/index.js
@@ -5,15 +5,15 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
 
 //这一句是连接上数据库
- var db = mongoose.connect('mongodb://localhost:27017/myDbs');
+var db = mongoose.connect('mongodb://localhost:27017/myDbs');
 
 //这里的myDbs是数据库的名字，不是表的名字
 
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api',store)
-app.listen(3000,() => {
+app.use('/api', store)
+app.listen(3000, () => {
     console.log('app listening on port 3000.')
 })
 
