df2c140b420f7a3c6935e696e6e0c12a9602d519
yangguang <yangguang@bilibili.com>
2018-05-26 15:39:57 +0800

    node.txt

--- demo/01CreateServer.js
+++ demo/01CreateServer.js
@@ -1,4 +1,5 @@
-// NodeJS本来的用途是编写高性能Web服务器。我们首先在这里重复一下官方文档里的例子，使用NodeJS内置的http模块简单实现一个HTTP服务器。
+// NodeJS本来的用途是编写高性能Web服务器。我们首先在这里重复一下官方文档里的例子，
+// 使用NodeJS内置的http模块简单实现一个HTTP服务器。
 const http = require('http');
 
 http.createServer(function (request, response) {
